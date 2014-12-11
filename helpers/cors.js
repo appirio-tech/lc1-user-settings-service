ba25ce978b4e723bc6c2c366aeffc3b4b9c18a46
Thabo <tha.bo.fletcher@gmail.com>
2014-12-10 22:36:49 -0800

    fix cors options

--- helpers/cors.js
+++ helpers/cors.js
@@ -10,9 +10,18 @@
 
     return function (req, res, next) {
       if (req.headers.origin && req.headers.origin.match(options.allow)) {
+        var methods = 'GET,PUT,POST,DELETE';
+        res.header('Allow', methods)
+        res.header('Access-Control-Allow-Methods', methods)
         res.header('Access-Control-Allow-Origin', req.headers.origin);
         res.header('Access-Control-Allow-Headers', 'Authorization,Content-Type,Accept,Origin,User-Agent,DNT,Cache-Control,X-Mx-ReqToken,Keep-Alive,X-Requested-With,If-Modified-Since');
         res.header('Access-Control-Allow-Credentials', 'true');
+
+        if (req.method == "OPTIONS") {
+          res.write(methods);
+          res.end();
+          return;
+        }
       }
       next();
     };
